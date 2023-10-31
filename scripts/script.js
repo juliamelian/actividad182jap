a015cb6f5de0f0649aed3bd7acb33cad8dc43721
Esteban Bentancor <ebentancor341@gmail.com>
2023-10-31 20:38:00 -0300

    borrar
    123

--- scripts/script.js
+++ scripts/script.js
@@ -7,3 +7,15 @@ const panel = document.getElementById('results');
 const urlGetListado = "https://65417f75f0b8287df1fe6ba5.mockapi.io/users";
 const urlGetListadoID = `https://65417f75f0b8287df1fe6ba5.mockapi.io/users/${id.value}`;
 
+botonDel.addEventListener('click', () => {
+    const idBorrar = document.getElementById('inputDelete').value;
+  
+    // Buscar el elemento en el panel por su ID
+    const elementoAEliminar = panel.querySelector(`#${idBorrar}`);
+  
+    if (elementoAEliminar) {
+      elementoAEliminar.remove();
+    } else {
+      console.log("Elemento no encontrado en el panel");
+    }
+  });
