c4fcac88574912c7d2d7b8afa5fe32147bbf98a2
avarageuser <gonzalezmoraleslucas@gmail.com>
2023-10-31 22:45:21 -0300

    PUT funcional

--- scripts/script.js
+++ scripts/script.js
@@ -7,27 +7,39 @@ const panel = document.getElementById('results');
 const urlGetListado = "https://65417f75f0b8287df1fe6ba5.mockapi.io/users";
 const urlGetListadoID = `https://65417f75f0b8287df1fe6ba5.mockapi.io/users/${id.value}`;
 
-// Obtén una referencia a tu botón "Modificar"
+
 const botonMod = document.getElementById('btnPut');
+const nameInput = document.getElementById('nameInput');
+const lastnameInput = document.getElementById('lastnameInput');
+const saveChangesButton = document.getElementById('saveChanges');
 
-// Agrega un controlador de evento al botón "Modificar"
-botonModificar.addEventListener('click', () => {
-    // Obtiene los valores ingresados por el usuario
-    const nombre = document.getElementById('nameInput').value;
-    const apellido = document.getElementById('lastnameInput').value;
-     
+botonMod.addEventListener('click', () => {
+    const idMOD = document.getElementById('inputPutId').value;
 
-    // Crea un objeto con los datos que deseas actualizar
+    // Realiza la solicitud GET para obtener los datos existentes del usuario con el ID especificado y luego los muestra en el modal
+    fetch(`${urlGetListado}/${idMOD}`)
+        .then(response => response.json())
+        .then(data => {
+            nameInput.value = data.name;
+            lastnameInput.value = data.lastname;
+        })
+        .catch(error => {
+            console.error('Error al obtener datos del usuario:', error);
+        });
+});
+
+// Cuando se hace clic en "Guardar" en el modal, realiza una solicitud PUT para actualizar los datos
+saveChangesButton.addEventListener('click', () => {
+    const idMOD = document.getElementById('inputPutId').value;
+    const nombre = nameInput.value;
+    const apellido = lastnameInput.value;
+
     const data = {
-        name: nameInput,
-        lastname: lastnameInput,
+        name: nombre,
+        lastname: apellido,
     };
 
-    // URL completa para el usuario específico
-    const url = `${url}/${lastnameInput}`;
-
-    // Realiza la solicitud PUT a la API
-    fetch(url, {
+    fetch(`${urlGetListado}/${idMOD}`, {
         method: 'PUT',
         headers: {
             'Content-Type': 'application/json'
@@ -36,10 +48,14 @@ botonModificar.addEventListener('click', () => {
     })
     .then(response => response.json())
     .then(data => {
+        panel.innerHTML = `Usuario actualizado: <br>ID: ${data.id}<br>Nombre: ${data.name}<br>Apellido: ${data.lastname}`
         console.log('Usuario actualizado:', data);
-    
     })
     .catch(error => {
+        panel.innerHTML = `Error al actualizar usuario: ${error}`
         console.error('Error al actualizar usuario:', error);
     });
 });
+
+panel.innerHTML = `Usuario actualizado: <br>ID: ${data.id}<br>Nombre: ${data.name}<br>Apellido ${data.lastname}`
+console.log('Usuario actualizado:', data);
